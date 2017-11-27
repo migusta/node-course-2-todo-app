1e98920ca1b0f72e914310c88b6ccda24bf7a7b9
Migusta <amigushina@gmail.com>
2017-11-28 00:11:32 +0300

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -47,7 +47,7 @@ describe('POST /todos', () => {
 
         Todo.find().then((todos) => {
           expect(todos.length).toBe(2);
-          done()
+          done();
         }).catch((e) => done(e));
       });
   });
@@ -89,5 +89,41 @@ describe("GET /todos/:id", () => {
       .expect(404)
       .end(done);
   });
-
 });
+
+describe("DELETE /todos/:id", () => {
+  it('should remove a todo', (done) => {
+    var hexid = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexid}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo._id).toBe(hexid);
+      })
+      .end((err,res) => {
+        if(err) {
+          return done(err);
+        }
+
+        Todo.findById(hexid).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if id is invalid', (done) => {
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(404)
+      .end(done);
+  });
+})
