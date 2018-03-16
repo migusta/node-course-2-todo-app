8727279694920e2e34fe98734e87d1faa123085c
Migusta <amigushina@gmail.com>
2018-03-16 15:08:22 +0300

    Convert some funsction to using async/await

--- server/server.js
+++ server/server.js
@@ -59,25 +59,48 @@ app.get('/todos/:id', authenticate, (req,res) => {
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
+// app.delete('/todos/:id', authenticate, (req, res) => {
+//   var id = req.params.id;
+
+//   if(!ObjectID.isValid(id)){
+//     return res.status(404).send();
+//   }
+
+//   Todo.findOneAndRemove(
+//     {
+//       _id: id,
+//       _creator: req.user._id
+//     }).then((todo) => {
+//     if(!todo){
+//       return res.status(404).send();
+//     }
+//     res.send({todo});
+//   }).catch((e) => {
+//     return res.status(400).send();
+//   });
+// });
+
+app.delete('/todos/:id', authenticate, async (req, res) => {
+  const id = req.params.id;
 
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-
-  Todo.findOneAndRemove(
+  try{
+    const todo = await Todo.findOneAndRemove(
     {
       _id: id,
       _creator: req.user._id
-    }).then((todo) => {
+    });
     if(!todo){
       return res.status(404).send();
     }
     res.send({todo});
-  }).catch((e) => {
-    return res.status(400).send();
-  });
+  }
+  catch(e){
+    return res.status(400).send(); 
+  }
+
 });
 
 app.patch('/todos/:id', authenticate, (req,res) => {
@@ -111,43 +134,82 @@ app.patch('/todos/:id', authenticate, (req,res) => {
 
 });
 
-app.post('/users',(req, res) => {
-  var body = _.pick(req.body, ['email','password']);
-  var user = new User(body);
+// app.post('/users',(req, res) => {
+//   var body = _.pick(req.body, ['email','password']);
+//   var user = new User(body);
 
-  user.save().then(() => {
-  //  res.send(user);
-  return user.generateAuthToken();
-}).then((token)=>{
-  res.header('x-auth',token).send(user);
-}).catch((e) => {
+//   user.save().then(() => {
+//   //  res.send(user);
+//   return user.generateAuthToken();
+// }).then((token)=>{
+//   res.header('x-auth',token).send(user);
+// }).catch((e) => {
+//     res.status(400).send(e);
+//   });
+// });
+
+app.post('/users', async (req, res) => {
+  
+  try{
+    const body = _.pick(req.body, ['email','password']);
+    const user = new User(body);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.header('x-auth',token).send(user);
+  }
+  catch(e){
     res.status(400).send(e);
-  });
+  }
 });
+
 
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email','password']);
+// app.post('/users/login', (req, res) => {
+//   var body = _.pick(req.body, ['email','password']);
 
-  User.findByCredentials(body.email, body.password).then((user) => {
-    user.generateAuthToken().then((token)=>{
-      res.header('x-auth',token).send(user);
-    });
-  }).catch((e) => {
+//   User.findByCredentials(body.email, body.password).then((user) => {
+//     user.generateAuthToken().then((token)=>{
+//       res.header('x-auth',token).send(user);
+//     });
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
+
+app.post('/users/login', async (req, res) => {
+  
+  try{
+    const body = _.pick(req.body, ['email','password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth',token).send(user);
+  }
+  catch (e) {
     res.status(400).send();
-  });
+  }
+
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
+// app.delete('/users/me/token', authenticate, (req, res) => {
+//   req.user.removeToken(req.token).then(() => {
+//     res.status(200).send();
+//   },() => {
+//     res.status(400).send();
+//   });
+// });
+
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try{
+    await req.user.removeToken(req.token);
     res.status(200).send();
-  },() => {
+  }
+  catch(e) {
     res.status(400).send();
-  });
+  }
 });
 
 app.listen(port, () => {
